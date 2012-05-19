ef96947049ccfce10203e298072d6e6bdebf02f0
Nano Taboada <nanotaboada@msn.com>
2012-05-19 04:47:48 -0300

    Added NUnit sample project

---
+++ Mono.Samples.NUnit/Mono.Samples.NUnit/src/ProgramTests.cs
@@ -0,0 +1,67 @@
+﻿#region License
+// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+#endregion
+
+using NUnit.Framework;
+
+namespace Mono.Samples.NUnit
+{
+    /// <remarks>
+    /// Basic rules for placing and naming tests
+    /// 
+    /// - For each class, create at least one class with the name [ClassName]Tests.
+    /// - For each method, create at least one test method with the following name: 
+    ///   [MethodName]_[StateUnderTest]_[ExpectedBehavior].
+    ///
+    /// Source: Osherove, Roy. The Art of Unit Testing. Manning, 2009. 28. Print.
+    /// </remarks>
+
+    [TestFixture]
+    public class ProgramTests
+    {
+        public Program program { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.program = new Program();
+        }
+
+        [Test]
+        public void GetPangram_ReturnValueIsNotNullOrEmpty_ReturnsTrue()
+        {
+            Assert.IsNotNullOrEmpty(program.GetPangram());
+        }
+
+        [Test]
+        public void GetPangram_ReturnValueEqualsExpectedString_ReturnsTrue()
+        {
+            string expected = "The quick brown fox jumps over the lazy dog.";
+            Assert.AreEqual(expected, program.GetPangram());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.program = null;
+        }
+    }
+}
