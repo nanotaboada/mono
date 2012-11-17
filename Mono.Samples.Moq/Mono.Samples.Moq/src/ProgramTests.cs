e23c48779bc71d0e09a25bbd83ef778eb050da00
Nano Taboada <nanotaboada@msn.com>
2012-11-17 15:00:58 -0300

    Added Moq sample project

---
+++ Mono.Samples.Moq/Mono.Samples.Moq/src/ProgramTests.cs
@@ -0,0 +1,63 @@
+#region License
+// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+#endregion
+
+using NUnit.Framework;
+using Moq;
+
+namespace Mono.Samples.Moq
+{
+    /// <remarks>
+    /// Let's assume that "IProgram" is an external (and sometimes unavailable)
+    /// API that we want to mock in order to properly test the remaining logic.
+    /// 
+    /// Notice we're not interested in testing its eventual implementation, but
+    /// just the contract. We're even actually going to modify the return value
+    /// of its "GetPangram" method to better illustrate the concept of mocking. 
+    /// </remarks>
+    
+    [TestFixture]
+    public class ProgramTests
+    {
+        public Mock<IProgram> mock;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.mock = new Mock<IProgram>();
+            this.mock.Setup(x => x.GetPangram()).Returns("Lorem ipsum dolor sit amet.");
+        }
+
+        [Test]
+        [TestCase("Lorem ipsum dolor sit amet.")]
+        public void GetPangram_ReturnValueEqualsExpectedString_ReturnsTrue(string expected)
+        {
+            Assert.AreEqual(this.mock.Object.GetPangram(), expected);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.mock = null;
+        }
+    }
+}
+
